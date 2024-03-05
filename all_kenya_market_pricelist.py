6dfbd2e9cba927711f1b276c5a42279ee43ea808
JOS-PROJECTS <162332651+JOS-PROJECTS@users.noreply.github.com>
2024-03-05 17:32:46 +0300

    Update all_kenya_market_pricelist.py

--- all_kenya_market_pricelist.py
+++ all_kenya_market_pricelist.py
@@ -34,28 +34,4 @@ for i in raw_data:
 print(stocks_all)
 
 
-#CODE TO CHECK LINE NUMBERS FOR DATA
-"""c=0
-toppers_table=[]
-titles=["class=head", "row r0", "row r1", "class=nm"]
-for i in page_lines:
-    if any(x in i for x in titles):
-        s=(i, "line no.", c)
-        toppers_table.append(s)
-    c=c+1
-print(page_lines[41:201])
-#print(toppers_table)
-"""
-
-#CODE TO CHECK FOR MATCHES IN PAGE LINES LIST
-"""matches = ["BAMB","EABL","SASINI LTD"]
-l=["SASN","NCBA","SCOM","WTK","EVRD","KCB","ABSA","CTUM","UMME","SLAM","SGL","SCBK","LKL","LBTY","ABSA","KCB","Military Expenditure","Inflation Rate MoM","Inflation Rate","Producer Prices Index YoY","Food Inflation YoY","Consumer Price Index"]
-
-for i in page_lines:
-    if any(x in i for x in matches):
-        print(i)
-    else:
-        continue
-"""
-
     
